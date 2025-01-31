4c726fca780a0b04ba9e656211ff87317966c419
Jake Atwell <jatwell@alexinc.com>
2025-01-31 13:52:51 -0500

    Include test case to call functions in db_package

---
+++ triangulation/tests/test_db_package.py
@@ -0,0 +1,48 @@
+import json
+import unittest
+
+from housing_passports.db_package import (
+    _get_session,
+    add_buildings,
+    add_images,
+    export_to_db,
+)
+
+
+class DBTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.db_url = "postgresql://postgres:1234@hpdb:5432/db_passport"
+        cls.session = _get_session(cls.db_url)
+        input_dir = "files_for_db"
+        cls.geomfile_fpath = input_dir + "/dominica_buildings/01_osm_buildings.geojson"
+        cls.trajectory_fpath = input_dir + "/trajectory.csv"
+        cls.parts_inference_fpath = input_dir + "/parts_inference_file.json"
+        cls.props_inference_fpath = input_dir + "/props_inference_file.json"
+        cls.parts_map_fpath = input_dir + "/parts_map_file.pbtxt"
+        cls.props_map_fpath = input_dir + "/props_map_file.pbtxt"
+
+    def test_add_buildings(self):
+        add_buildings(self.geomfile_fpath, self.session)
+        self.session.commit()
+
+    def test_add_images(self):
+        add_images(self.trajectory_fpath, self.session)
+        self.session.commit()
+
+
+class Tests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.db_url = "postgresql://postgres:1234@hpdb:5432/db_passport"
+        input_dir = "files_for_db"
+        cls.geomfile_fpath = input_dir + "/dominica_buildings/01_osm_buildings.geojson"
+        cls.trajectory_fpath = input_dir + "/trajectory.csv"
+        cls.parts_inference_fpath = input_dir + "/parts_inference_file.json"
+        cls.props_inference_fpath = input_dir + "/props_inference_file.json"
+        cls.parts_map_fpath = input_dir + "/parts_map_file.pbtxt"
+        cls.props_map_fpath = input_dir + "/props_map_file.pbtxt"
+
+    def test_load_parts_inference_fpath(self):
+        with open(self.parts_inference_fpath, "r") as json_file:
+            data = json.load(json_file)
