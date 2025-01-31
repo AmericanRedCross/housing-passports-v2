357842555edce906324be57b3fa615b71e8b72fc
Jake Atwell <jatwell@alexinc.com>
2025-01-31 14:34:49 -0500

    Change bbox and org_id for getting points

--- pre_processing/pre_processing.sh
+++ pre_processing/pre_processing.sh
@@ -8,13 +8,13 @@ spherical2imagesdocker="docker run -v $PWD:/mnt/ -e MAPILLARY_ACCESS_TOKEN=$MAPI
 
 
 $geokitdocker get_mapillary_points \
-   --input_aoi=-61.493225,15.204024,-61.232986,15.642874 \
-   --organization_ids=276431331814934 \
-   --timestamp_from=1672531200000 \
+   --input_aoi=112.748232,-7.304144,112.755635,-7.298302 \
+   --organization_ids=1606999786835974 \
    --only_pano \
    --output_file_point=${outputDir}/mapillary_points_panoramic__pano.geojson \
    --output_file_sequence=${outputDir}/mapillary_sequences_panoramic__pano.geojson
 
+   # --timestamp_from=1729641600000 \
 
 $spherical2imagesdocker clip_mapillary_pano \
   --input_file_points=${outputDir}/mapillary_points_panoramic__pano__pano.geojson \
